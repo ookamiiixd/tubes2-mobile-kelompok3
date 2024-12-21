5207edcdd96c95b8586183558e1f775a3adc32cb
Afsha <afshaa222@gmail.com>
2024-12-22 02:46:18 +0700

    onvert add vote json to model class ( coding ) ada 2 yaitu request body dan responses

--- lib/data/favorite/datasource/favorite_datasource.dart
+++ lib/data/favorite/datasource/favorite_datasource.dart
@@ -1,10 +1,9 @@
-import 'package:tubes2_uas_kel9/core/api_services/dio_module.dart';
-import 'package:tubes2_uas_kel9/core/api_services/endpoint.dart';
-import 'package:tubes2_uas_kel9/data/favorite/responsesmodel/favorite_responses_model_delete.dart';
-import 'package:tubes2_uas_kel9/data/favorite/responsesmodel/favorite_responses_model_get.dart';
-import 'package:tubes2_uas_kel9/data/favorite/responsesmodel/favorite_responses_model_post.dart';
-import 'package:tubes2_uas_kel9/data/favorite/requestsmodel/favorite_requests_model.dart';
-import 'package:tubes2_uas_kel9/data/favorite/responsesmodel/favorite_responses_model.dart';
+import 'package:tubes2-mobile-kelompok3/core/api_services/dio_module.dart';
+import 'package:tubes2-mobile-kelompok3/core/api_services/endpoint.dart';
+import 'package:tubes2-mobile-kelompok3/data/favorite/responsesmodel/favorite_responses_model_delete.dart';
+import 'package:tubes2-mobile-kelompok3/data/favorite/responsesmodel/favorite_responses_model_get.dart';
+import 'package:tubes2-mobile-kelompok3/data/favorite/responsesmodel/favorite_responses_model_post.dart';
+import 'package:tubes2-mobile-kelompok3/data/favorite/requestsmodel/favorite_requests_model.dart';
 
 class FavoriteDatasource {
   final HttpManager httpManager = HttpManager();
@@ -16,7 +15,7 @@ class FavoriteDatasource {
         method: HttpMethods.get,
         useAuth: true,
       );
-      // PrintLog.printLog("getFavourites datasource responses : ${response}");
+
       if (response['statusCode'] == 200) {
         final List<dynamic> data = response['data'];
         return data
@@ -24,7 +23,7 @@ class FavoriteDatasource {
             .toList();
       } else {
         throw Exception(
- 'Failed to load favourites : ${response['statusMessage']}');
+            'Failed to load favourites: ${response['statusMessage']}');
       }
     } catch (e) {
       throw Exception('Failed to load favourites: $e');
@@ -40,25 +39,26 @@ class FavoriteDatasource {
         useAuth: true,
         body: data.toJson(),
       );
-      // PrintLog.printLog("getFavourites datasource responses : ${response}");
+
       if (response['statusCode'] == 200) {
         return FavouritesResponsesModelPost.fromJson(response['data']);
       } else {
         return null;
       }
     } catch (e) {
-      throw Exception('Failed to load favourites: $e');
+      throw Exception('Failed to create favourites: $e');
     }
   }
 
   Future<FavouritesResponsesModelDelete?> deleteFavourites(
       int favoriteId) async {
-try {
+    try {
       final response = await httpManager.restRequest(
         url: ApiConstants.favByIdGetEndpoint(favoriteId),
         method: HttpMethods.delete,
         useAuth: true,
       );
+
       final message = response['message'];
       final statusMessage = response['statusMessage'];
 
@@ -67,29 +67,14 @@ try {
             response['data'] is Map<String, dynamic>) {
           return FavouritesResponsesModelDelete.fromJson(response['data']);
         } else {
-          return FavouritesResponsesModelDelete(message: message ?? "SUCCESS");
+          return FavouritesResponsesModelDelete(
+              message: message ?? "SUCCESS");
         }
       } else {
         return null;
       }
     } catch (e) {
-      throw Exception('Failed to load favourites: $e');
+      throw Exception('Failed to delete favourites: $e');
     }
   }
-  Future<FavouritesResponsesModelSearch?> searchFavourites(int favoriteId) async {
-    try {
-      final response = await httpManager.restRequest(
-        url: ApiConstants.favByIdGetEndpoint(favoriteId),
-        method: HttpMethods.get,
-        useAuth: true,
-      );
-      if (response['statusCode'] == 200) {
-        return FavouritesResponsesModelSearch.fromJson(response['data']);
-} else {
-        return null;
-      }
-    } catch (e) {
-      throw Exception('Failed to load favourites: $e');
-    }
-  }
 }
