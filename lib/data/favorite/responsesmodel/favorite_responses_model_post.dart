94c39aa19339dc5f748af1662ffcdbdf4f29afc7
arjunkhoirul <aarjjun4@gmail.com>
2024-12-23 23:53:29 +0700

    feat : convert add fav json to model class ( coding ) ada 2 yaitu request body dan responses

---
+++ lib/data/favorite/responsesmodel/favorite_responses_model_post.dart
@@ -0,0 +1,30 @@
+// To parse this JSON data, do
+//
+//     final favoritesResponsesModelPost = favoritesResponsesModelPostFromJson(jsonString);
+
+import 'package:meta/meta.dart';
+import 'dart:convert';
+
+FavoritesResponsesModelPost favoritesResponsesModelPostFromJson(String str) => FavoritesResponsesModelPost.fromJson(json.decode(str));
+
+String favoritesResponsesModelPostToJson(FavoritesResponsesModelPost data) => json.encode(data.toJson());
+
+class FavoritesResponsesModelPost {
+    final String message;
+    final int id;
+
+    FavoritesResponsesModelPost({
+        required this.message,
+        required this.id,
+    });
+
+    factory FavoritesResponsesModelPost.fromJson(Map<String, dynamic> json) => FavoritesResponsesModelPost(
+        message: json["message"],
+        id: json["id"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "message": message,
+        "id": id,
+    };
+}
